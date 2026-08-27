b6c200bde66889577a76b99ab4d68e452333e94b
Alessandro Moreira <alessandrotibia664@gmail.com>
2026-08-27 16:38:22 -0300

    Update editor_anotacoes.py

--- GLADE/editor_anotacoes.py
+++ GLADE/editor_anotacoes.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# Nomes: Alessandro da Silva Moreira, Matheus Mendes Francisco e Leon Antonio Pereira Batista
+# Turma: 2º Informática
+
 import os
 import gi
 
@@ -26,25 +29,34 @@ class Aplicacao:
         self.alteracoes = self.construtor.get_object('salvamento')
         self.caracteres = 0
         self.palavras = 0
+        self.txt_titulo = ''
+        self.txt_anotacao = ''
+        self.alterou = False
 
         self.janela.show_all()
 
     def salvar(self, componente=None, dados=None):
-        title = self.entrada_titulo.get_text()
-        self.titulo.set_markup(f"<b>Editor de Anotações - {title}</b>")
-        self.alteracoes.set_markup(f'<span foreground="green">Alterações salvas</span>')
+        if not self.alterou:
+            self.alteracoes.set_markup(f'<span foreground="red">Nada foi escrito</span>')
+        else:
+            title = self.entrada_titulo.get_text()
+            self.titulo.set_markup(f"<big><b>Editor de Anotações - {title}</b></big>")
+            self.alteracoes.set_markup(f'<span foreground="green">Alterações salvas</span>')
 
     def limpar(self, componente=None, dados=None):
+        self.alterou = False
         self.entrada_titulo.set_text("")
         self.entrada_anotacao.set_text("")
-        self.titulo.set_markup(f"<b>Editor de Anotações - sem título *</b>")
+        self.titulo.set_markup(f"<big><b>Editor de Anotações - sem título *</b></big>")
         self.lbl_caracteres.set_markup(f"<small>0 caracteres - 0 palavras</small>")
         self.alteracoes.set_markup(f'<span foreground="red">Alterações não salvas</span>')
 
-    def incrementar(self, componente=None, dados=None):
-        texto_contagem = len(self.entrada_titulo.get_text().split(" ")) + len(self.entrada_anotacao.get_text().split(" "))
-        self.palavras += texto_contagem
-        self.caracteres += 1
+    def atualizar_textos(self, componente=None, dados=None):
+        self.alterou = True
+        self.txt_titulo = self.entrada_titulo.get_text()
+        self.txt_anotacao = self.entrada_anotacao.get_text()
+        self.caracteres = len(self.txt_titulo) + len(self.txt_anotacao)
+        self.palavras = len(self.txt_titulo.split()) + len(self.txt_anotacao.split())
         self.lbl_caracteres.set_text(f"{self.caracteres} caracteres - {self.palavras} palavras")
 
     def fechar(self, componente=None, dados=None):
